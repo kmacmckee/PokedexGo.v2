d7462f1724c322093cae6f9e4b7721b6e65a4cfa
Kobe McKee <kobe.mckee@gmail.com>
2019-07-29 16:03:14 -0600

    Initial commit

---
+++ PokdexGo.v2/PokdexGo.v2Tests/PokdexGo_v2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  PokdexGo_v2Tests.swift
+//  PokdexGo.v2Tests
+//
+//  Created by Kobe McKee on 7/29/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import XCTest
+@testable import PokdexGo_v2
+
+class PokdexGo_v2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
