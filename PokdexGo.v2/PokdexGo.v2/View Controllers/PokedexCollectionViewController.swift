9f4fb2da5af053f57cbe0551d41f6c20a4811bc0
Kobe McKee <kobe.mckee@gmail.com>
2019-08-02 15:00:44 -0600

    Pre presentation commit/working build

--- PokdexGo.v2/PokdexGo.v2/View Controllers/PokedexCollectionViewController.swift
+++ PokdexGo.v2/PokdexGo.v2/View Controllers/PokedexCollectionViewController.swift
@@ -87,11 +87,11 @@ class PokedexCollectionViewController: UICollectionViewController, UICollectionV
         
         let bannerWidth = navController.navigationBar.frame.size.width
         let bannerHeight = navController.navigationBar.frame.size.height
-        
+
         let bannerX = bannerWidth / 2 - image.size.width / 2
         let bannerY = bannerHeight / 2 - image.size.height / 2
-        
-        
+
+
         imageView.frame = CGRect(x: bannerX, y: bannerY, width: bannerWidth, height: bannerHeight)
         imageView.contentMode = .scaleAspectFit
         imageView.addShadow()
